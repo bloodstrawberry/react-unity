3e6370496b17bf7490f9b8e17ddb951217bfd6be
bloodstrawberry <vvv3334@hanmail.net>
2023-03-22 00:47:50 +0900

    first submit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import { Unity, useUnityContext } from "react-unity-webgl";
 
 function App() {
+  const { unityProvider } = useUnityContext({
+    loaderUrl: "react-unity/build/html-hosting.loader.js",
+    dataUrl: "react-unity/build/html-hosting.data",
+    frameworkUrl: "react-unity/build/html-hosting.framework.js",
+    codeUrl: "react-unity/build/html-hosting.wasm",
+  });
+
+  // const { unityProvider } = useUnityContext({
+  //   loaderUrl: "build/html-hosting.loader.js",
+  //   dataUrl: "build/html-hosting.data",
+  //   frameworkUrl: "build/html-hosting.framework.js",
+  //   codeUrl: "react-unity/build/html-hosting.wasm",
+  // });
+
   return (
     <div className="App">
       <header className="App-header">
+        <Unity unityProvider={unityProvider} />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,6 +34,7 @@ function App() {
         >
           Learn React
         </a>
+        
       </header>
     </div>
   );
